ba8ed7b7d94ef6e9f453d51ab91c2c436cf6a564
glaw <glaw@localhost>
2017-01-16 18:16:30 -0700

    Making routing recursive

--- src/main/java/com/glaw/FolderProcessor.java
+++ src/main/java/com/glaw/FolderProcessor.java
@@ -9,6 +9,13 @@ public class FolderProcessor {
   ArrayList<File> createFileList(File folder){
     ArrayList<File> list = new ArrayList<File>();
     list.addAll(Arrays.asList(folder.listFiles()));
+    ArrayList<File> subList = new ArrayList<File>();
+    for(File file : list ){
+      if(file.isDirectory()){
+        subList.addAll(createFileList(file));
+      }
+    }
+    list.addAll(subList);
     return list;
   }
   
